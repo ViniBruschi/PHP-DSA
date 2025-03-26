8bca49eec272ac7fe65661a8fa5244fee95e8457
Vinicius <vinicius.bruschi@catwork.com.br>
2025-03-26 09:29:15 -0400

    Criação da função de append para listas encadeadas

--- LinkedList.php
+++ LinkedList.php
@@ -21,9 +21,19 @@ class LinkedList {
         $this->tail = $new_node;
         $this->length = 1;
     }
+
+    public function append($value) {
+        $new_node = new Node($value);
+        $this->tail->next = $new_node;
+        $this->tail = $new_node;
+        $this->length++;
+    }
 }
 
 $my_linked_list = new LinkedList(4);
+$my_linked_list->append(5);
+$my_linked_list->append(10);
+$my_linked_list->append(6);
 
 echo 'Head: ' . $my_linked_list->head->value . "\n";
 echo 'Tail: ' . $my_linked_list->tail->value . "\n";
